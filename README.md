e3d026175590ee5673eb097fd9d7bb006f573413
Champoolwoo <golf_zet@hotmail.com>
2016-03-27 03:26:05 -0700

    edit test

--- README.md
+++ README.md
@@ -1,22 +1,21 @@
 ## Step use Web Ban9Jin
 ## Harry Potter
-### 1. Clone
+### 1. Clone ทำการ Clone Code ต่างๆมาที่เครื่องคอม
 ```
 $ git clone https://github.com/Champoolwoo/midtest.git
 ```
-### 2. Install
+### 2. Install ทำการติดตั้ง Packet ที่ต้องใช้
 ```
 $ npm install
 ```
-### 3. Run
+### 3. Run ทำการ run
 ```
 $ npm start
 ```
-### 4. Test
+### 4. Test ทำการ run test
 ```
 $ npm test
 ```
 ---
-### 5. On Heroku
+### 5. ลิ้งค์เว็บ Heroku
 [http://ban9jinbook.herokuapp.com/](http://ban9jinbook.herokuapp.com/)
-
--- test/test.js
+++ test/test.js
@@ -4,17 +4,93 @@ require('mocha-generators').install()
 var Nightmare = require('nightmare')
 var expect = require('chai').expect // jshint ignore:line
 
-describe('test Ban9Jin', function () {
-  it('Select 1 Book', function * () {
+describe('Test Web Ban9Jin', function () {
+  it('1-2 1 book', function * () {
     this.timeout(30000)
     var nightmare = Nightmare()
     var link = yield nightmare
       .goto('http://localhost:5000')
       .click('#b1')
-      .wait(3000)
+      .click('#b2')
+
       .evaluate(function () {
         return document.querySelector('#summary').innerHTML
       })
-    expect(link).to.equal('100')
+    expect(link).to.equal('180')
+  })
+
+  it('1-7 1 book', function * () {
+    this.timeout(30000)
+    var nightmare = Nightmare()
+    var link = yield nightmare
+      .goto('http://localhost:5000')
+      .click('#b1')
+      .click('#b2')
+      .click('#b3')
+      .click('#b4')
+      .click('#b5')
+      .click('#b6')
+      .click('#b7')
+      .evaluate(function () {
+        return document.querySelector('#summary').innerHTML
+      })
+    expect(link).to.equal('280')
+  })
+
+  it('1-7 2 book', function * () {
+    this.timeout(30000)
+    var nightmare = Nightmare()
+    var link = yield nightmare
+      .goto('http://localhost:5000')
+      .click('#b1')
+      .click('#b2')
+      .click('#b3')
+      .click('#b4')
+      .click('#b5')
+      .click('#b6')
+      .click('#b7')
+      .click('#b1')
+      .click('#b2')
+      .click('#b3')
+      .click('#b4')
+      .click('#b5')
+      .click('#b6')
+      .click('#b7')
+      .evaluate(function () {
+        return document.querySelector('#summary').innerHTML
+      })
+    expect(link).to.equal('560')
+  })
+
+  it('1 buy 3,2 buy 2,3 buy 1', function * () {
+    this.timeout(30000)
+    var nightmare = Nightmare()
+    var link = yield nightmare
+      .goto('http://localhost:5000')
+      .click('#b1')
+      .click('#b1')
+      .click('#b1')
+      .click('#b2')
+      .click('#b2')
+      .click('#b3')
+      .evaluate(function () {
+        return document.querySelector('#summary').innerHTML
+      })
+    expect(link).to.equal('520')
+  })
+
+  it('1 buy 2,2 buy 2', function * () {
+    this.timeout(30000)
+    var nightmare = Nightmare()
+    var link = yield nightmare
+      .goto('http://localhost:5000')
+      .click('#b1')
+      .click('#b1')
+      .click('#b2')
+      .click('#b2')
+      .evaluate(function () {
+        return document.querySelector('#summary').innerHTML
+      })
+    expect(link).to.equal('360')
   })
 })
